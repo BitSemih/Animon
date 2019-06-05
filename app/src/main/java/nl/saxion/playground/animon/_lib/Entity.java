99b0d5dc27621275677601a11e31c055c7225649
BitSemih <464543@student.saxion.nl>
2019-06-05 14:05:56 +0200

    Added comment to lib change

--- app/src/main/java/nl/saxion/playground/animon/_lib/Entity.java
+++ app/src/main/java/nl/saxion/playground/animon/_lib/Entity.java
@@ -14,6 +14,7 @@ abstract public class Entity implements Comparable<Entity>, Serializable {
     // Static variable that provides the next `id`.
     private static int count = 0;
 
+    // Static state var that decides what is to be rendered
     public static int state = 0;
 
     // The constructor assigns an id that is used for ordering draws.
