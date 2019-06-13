3029999d15bdca5eed8b47abc70e97e8e3114fa2
BitSemih <semihoren@hotmail.nl>
2019-06-13 19:11:39 +0200

    Added directional animations

--- app/src/main/java/nl/saxion/playground/animon/game/Player.java
+++ app/src/main/java/nl/saxion/playground/animon/game/Player.java
@@ -29,11 +29,13 @@ public class Player extends Entity implements KeyListener {
     private float playerOffsetY;
     private float cameraCenterY;
     private String walkDirection = "right";
+    private int animationCount = 1;
+    private boolean walking = false;
 
-    static private final int[] spriteResourceIds = {0, R.drawable.s_player};
+    static private final int[] spriteResourceIds = {0, R.drawable.s_player, R.drawable.s_player_down, R.drawable.s_player_up, R.drawable.s_player_walk0, R.drawable.s_player_walk1, R.drawable.s_player_up_walk0, R.drawable.s_player_up_walk1, R.drawable.s_player_down_walk0, R.drawable.s_player_down_walk1};
 
     // When resources are first used, the decoded Bitmap is written to this array, as a cache.
-    static private Bitmap bitmapWalkRight, bitmapWalkLeft;
+    static private Bitmap bitmapFaceRight, bitmapWalkRight1, bitmapWalkRight2, bitmapFaceLeft, bitmapWalkLeft1, bitmapWalkLeft2, bitmapFaceUp, bitmapFaceDown, bitmapWalkUp1, bitmapWalkUp2, bitmapWalkDown1, bitmapWalkDown2;
 
     private ArrayList<Animon> animonArrayList = new ArrayList<>();
 
@@ -51,8 +53,8 @@ public class Player extends Entity implements KeyListener {
 
         rightBoundary = Map.getWidth() - 10;
         leftBoundary = 7;
-        upperBoundary = (int) (game.getHeight()/2) + 1;
-        downBoundary = Map.getHeight() - (int) (game.getHeight()/2);
+        upperBoundary = (int) (game.getHeight() / 2) + 1;
+        downBoundary = Map.getHeight() - (int) (game.getHeight() / 2);
 
         if (animonArrayList.size() < 1) {
             animonArrayList.add(new Chicken(this.game, "Chicken wing", 50, 5, 0));
@@ -64,18 +66,97 @@ public class Player extends Entity implements KeyListener {
         super.draw(gv);
 
         //Draw the player sprite
-        if (bitmapWalkLeft == null || bitmapWalkRight == null){
+        if (bitmapFaceRight == null) {
             Matrix matrix = new Matrix();
             matrix.preScale(-1.0f, 1.0f);
-            bitmapWalkRight = gv.getBitmapFromResource(spriteResourceIds[1]);
-            bitmapWalkLeft = Bitmap.createBitmap(bitmapWalkRight, 0, 0, bitmapWalkRight.getWidth(), bitmapWalkRight.getHeight(), matrix, true);
+
+            //Players stands still
+            bitmapFaceRight = gv.getBitmapFromResource(spriteResourceIds[1]);
+            bitmapFaceDown = gv.getBitmapFromResource(spriteResourceIds[2]);
+            bitmapFaceUp = gv.getBitmapFromResource(spriteResourceIds[3]);
+            bitmapFaceLeft = Bitmap.createBitmap(bitmapFaceRight, 0, 0, bitmapFaceRight.getWidth(), bitmapFaceRight.getHeight(), matrix, true);
+
+            //Walk right animation
+            bitmapWalkRight1 = gv.getBitmapFromResource(spriteResourceIds[4]);
+            bitmapWalkRight2 = gv.getBitmapFromResource(spriteResourceIds[5]);
+
+            //Walk left animation
+            bitmapWalkLeft1 = Bitmap.createBitmap(bitmapWalkRight1, 0, 0, bitmapFaceRight.getWidth(), bitmapFaceRight.getHeight(), matrix, true);
+            bitmapWalkLeft2 = Bitmap.createBitmap(bitmapWalkRight2, 0, 0, bitmapFaceRight.getWidth(), bitmapFaceRight.getHeight(), matrix, true);
+
+            //Walk up animation
+            bitmapWalkUp1 = gv.getBitmapFromResource(spriteResourceIds[6]);
+            bitmapWalkUp2 = gv.getBitmapFromResource(spriteResourceIds[7]);
+
+            //Walk down animation
+            bitmapWalkDown1 = gv.getBitmapFromResource(spriteResourceIds[8]);
+            bitmapWalkDown2 = gv.getBitmapFromResource(spriteResourceIds[9]);
         }
 
-        if (walkDirection.equals("left")){
-            gv.drawBitmap(bitmapWalkLeft, playerOffsetX, playerOffsetY, 1, 1);
-        } else {
-            gv.drawBitmap(bitmapWalkRight, playerOffsetX, playerOffsetY, 1, 1);
+        //Look which direction the player is walking
+        switch (walkDirection) {
+            case "left":
+                if (!walking) {
+                    //Standing still
+                    gv.drawBitmap(bitmapFaceLeft, playerOffsetX, playerOffsetY, 1, 1);
+                } else {
+                    //Walk animation
+                    if (animationCount == 1) {
+                        gv.drawBitmap(bitmapWalkLeft1, playerOffsetX, playerOffsetY, 1, 1);
+                        animationCount++;
+                    } else {
+                        gv.drawBitmap(bitmapWalkLeft2, playerOffsetX, playerOffsetY, 1, 1);
+                        animationCount--;
+                    }
+                }
+                break;
+            case "right":
+                if (!walking) {
+                    //Standing still
+                    gv.drawBitmap(bitmapFaceRight, playerOffsetX, playerOffsetY, 1, 1);
+                } else {
+                    //Walk animation
+                    if (animationCount == 1) {
+                        gv.drawBitmap(bitmapWalkRight1, playerOffsetX, playerOffsetY, 1, 1);
+                        animationCount++;
+                    } else {
+                        gv.drawBitmap(bitmapWalkRight2, playerOffsetX, playerOffsetY, 1, 1);
+                        animationCount--;
+                    }
+                }
+                break;
+            case "up":
+                if (!walking) {
+                    //Standing still
+                    gv.drawBitmap(bitmapFaceUp, playerOffsetX, playerOffsetY, 1, 1);
+                } else {
+                    //Walk animation
+                    if (animationCount == 1) {
+                        gv.drawBitmap(bitmapWalkUp1, playerOffsetX, playerOffsetY, 1, 1);
+                        animationCount++;
+                    } else {
+                        gv.drawBitmap(bitmapWalkUp2, playerOffsetX, playerOffsetY, 1, 1);
+                        animationCount--;
+                    }
+                }
+                break;
+            case "down":
+                if (!walking) {
+                    //Standing still
+                    gv.drawBitmap(bitmapFaceDown, playerOffsetX, playerOffsetY, 1, 1);
+                } else {
+                    //Walk animation
+                    if (animationCount == 1) {
+                        gv.drawBitmap(bitmapWalkDown1, playerOffsetX, playerOffsetY, 1, 1);
+                        animationCount++;
+                    } else {
+                        gv.drawBitmap(bitmapWalkDown2, playerOffsetX, playerOffsetY, 1, 1);
+                        animationCount--;
+                    }
+                }
+                break;
         }
+        walking = false;
     }
 
     @Override
@@ -85,17 +166,18 @@ public class Player extends Entity implements KeyListener {
         }
         if (x >= rightBoundary || playerOffsetX < 7) {
             if (playerOffsetX < game.getWidth() - 1.2f) {
-                playerOffsetX += 0.2;
+                playerOffsetX += 0.1f;
             }
         } else {
-            if (playerOffsetX != 7){
+            if (playerOffsetX != 7) {
                 playerOffsetX = 7;
             }
             if (!collision.checkForCollision(this.x + 0.3f, this.y, 0)) {
-                x += 0.2;
+                x += 0.1f;
             }
         }
         this.walkDirection = "right";
+        this.walking = true;
     }
 
     @Override
@@ -105,17 +187,18 @@ public class Player extends Entity implements KeyListener {
         }
         if (x <= leftBoundary || playerOffsetX > 7) {
             if (playerOffsetX > 0) {
-                playerOffsetX -= 0.2;
+                playerOffsetX -= 0.1f;
             }
         } else {
-            if (playerOffsetX != 7){
+            if (playerOffsetX != 7) {
                 playerOffsetX = 7;
             }
             if (!collision.checkForCollision(this.x - 0.2f, this.y, 0)) {
-                x -= 0.2;
+                x -= 0.1f;
             }
         }
         this.walkDirection = "left";
+        this.walking = true;
     }
 
     @Override
@@ -126,15 +209,17 @@ public class Player extends Entity implements KeyListener {
 
         if (y <= upperBoundary || playerOffsetY > cameraCenterY) {
             if (playerOffsetY > 0) {
-                playerOffsetY -= 0.2;
+                playerOffsetY -= 0.1f;
             }
         } else {
-            if (playerOffsetY != cameraCenterY){
+            if (playerOffsetY != cameraCenterY) {
                 playerOffsetY = cameraCenterY;
             }
             if (!collision.checkForCollision(this.x, this.y - 0.2f, 1)) {
-                y -= 0.2;
+                y -= 0.1f;
             }
+            this.walkDirection = "up";
+            this.walking = true;
         }
     }
 
@@ -146,15 +231,17 @@ public class Player extends Entity implements KeyListener {
 
         if (y >= downBoundary || playerOffsetY < cameraCenterY) {
             if (playerOffsetY < game.getHeight() - 1.2f) {
-                playerOffsetY += 0.2;
+                playerOffsetY += 0.1f;
             }
         } else {
-            if (playerOffsetY != cameraCenterY){
+            if (playerOffsetY != cameraCenterY) {
                 playerOffsetY = cameraCenterY;
             }
             if (!collision.checkForCollision(this.x, this.y + 1.1f, 1)) {
-                y += 0.2;
+                y += 0.1f;
             }
+            this.walkDirection = "down";
+            this.walking = true;
         }
     }
 
