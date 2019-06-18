99e780dcaa09de1ea332658f896c9e641c82aae9
MeesMaas <meesmaas999@gmail.com>
2019-06-18 13:52:10 +0200

    Merge changing movement into master and this time right

--- app/src/main/java/nl/saxion/playground/animon/game/Player.java
+++ app/src/main/java/nl/saxion/playground/animon/game/Player.java
@@ -1,6 +1,6 @@
 package nl.saxion.playground.animon.game;
 
-import android.graphics.Bitmap;
+import android.graphics.RectF;
 
 import java.util.ArrayList;
 
@@ -18,36 +18,39 @@ public class Player extends Entity implements KeyListener {
     private float y;
     private Collision collision;
 
+    private Game game;
+
     private int rightBoundary = 0;
     private int leftBoundary = 0;
     private int upperBoundary = 0;
     private int downBoundary = 0;
-
-
-    static private final int[] spriteResourceIds = {0, R.drawable.s_player};
-
-    // When resources are first used, the decoded Bitmap is written to this array, as a cache.
-    static private Bitmap[] spriteBitmaps;
-
-    private Game game;
+    private float playerOffsetX;
+    private float playerOffsetY;
+    private float cameraCenterY;
+    private String walkDirection = "right";
+    private int animationCount = 1;
+    private boolean walking = false;
+    private float frame = 0;
+    private SpriteSheet player_sheet;
 
     private ArrayList<Animon> animonArrayList = new ArrayList<>();
 
     public Player(Game game, Collision collision) {
-        //TODO check if the player already has an instance
-        spriteBitmaps = new Bitmap[spriteResourceIds.length];
 
         this.game = game;
         this.collision = collision;
         this.game.getEntity(KeyEntity.class).addKeyListener(this);
+        this.playerOffsetX = 7;
+        this.playerOffsetY = (game.getHeight() / 2) + 1;
+        x = 14;
+        y = (int) (game.getHeight() / 2) + 1;
 
-        x = 7;
-        y = (int) (game.getHeight()/2) + 1;
+        cameraCenterY = (int) playerOffsetY;
 
-        rightBoundary = Map.getWidth() - 7;
+        rightBoundary = Map.getWidth() - 10;
         leftBoundary = 7;
-        upperBoundary = (int) (game.getHeight()/2) + 1;
-        downBoundary = Map.getHeight() - (int) (game.getHeight()/2);
+        upperBoundary = (int) (game.getHeight() / 2) + 1;
+        downBoundary = Map.getHeight() - (int) (game.getHeight() / 2);
 
         if (animonArrayList.size() < 1) {
             animonArrayList.add(new Chicken(this.game, "Chicken wing", 50, 5, 0));
@@ -55,56 +58,170 @@ public class Player extends Entity implements KeyListener {
     }
 
     @Override
-    public void draw(GameView gv) {
-        super.draw(gv);
+    public void tick() {
+        if (walking) {
+            frame += 0.03f;
+            if ((int) frame % 2 == 0) {
+                animationCount = 0;
+            } else if ((int) frame % 2 == 1) {
+                animationCount = 1;
+            }
+        }
+    }
 
-        //Draw the player sprite
-        spriteBitmaps[1] = gv.getBitmapFromResource(spriteResourceIds[1]);
-        gv.drawBitmap(spriteBitmaps[1], 7,(int)(game.getHeight()/2) + 1, 1, 1);
+    @Override
+    public void draw(GameView gv) {
+
+        if (player_sheet == null) {
+            //Load in player sheet
+            player_sheet = SpriteSheet.createSheetFromColumnsAndRows(gv.getBitmapFromResource(R.drawable.s_player_sheet),12, 1);
+        }
+
+        //Look which direction the player is walking
+        switch (walkDirection) {
+            case "left":
+                if (!walking) {
+                    //Standing still
+                    player_sheet.drawFrame(11, gv.getCanvas(), new RectF(playerOffsetX, playerOffsetY, playerOffsetX + 1, playerOffsetY + 1));
+                } else {
+                    //Walk animation
+                    if (animationCount == 0) {
+                        player_sheet.drawFrame(9, gv.getCanvas(), new RectF(playerOffsetX, playerOffsetY, playerOffsetX + 1, playerOffsetY + 1));
+                    } else {
+                        player_sheet.drawFrame(10, gv.getCanvas(), new RectF(playerOffsetX, playerOffsetY, playerOffsetX + 1, playerOffsetY + 1));
+                    }
+                }
+                break;
+            case "right":
+                if (!walking) {
+                    //Standing still
+                    player_sheet.drawFrame(0, gv.getCanvas(), new RectF(playerOffsetX, playerOffsetY, playerOffsetX + 1, playerOffsetY + 1));
+                } else {
+                    //Walk animation
+                    if (animationCount == 0) {
+                        player_sheet.drawFrame(1, gv.getCanvas(), new RectF(playerOffsetX, playerOffsetY, playerOffsetX + 1, playerOffsetY + 1));
+                    } else {
+                        player_sheet.drawFrame(2, gv.getCanvas(), new RectF(playerOffsetX, playerOffsetY, playerOffsetX + 1, playerOffsetY + 1));
+                    }
+                }
+                break;
+            case "up":
+                if (!walking) {
+                    //Standing still
+                    player_sheet.drawFrame(6, gv.getCanvas(), new RectF(playerOffsetX, playerOffsetY, playerOffsetX + 1, playerOffsetY + 1));
+
+                } else {
+                    //Walk animation
+                    if (animationCount == 0) {
+                        player_sheet.drawFrame(7, gv.getCanvas(), new RectF(playerOffsetX, playerOffsetY, playerOffsetX + 1, playerOffsetY + 1));
+
+                    } else {
+                        player_sheet.drawFrame(8, gv.getCanvas(), new RectF(playerOffsetX, playerOffsetY, playerOffsetX + 1, playerOffsetY + 1));
+
+                    }
+                }
+                break;
+            case "down":
+                if (!walking) {
+                    //Standing still
+                    player_sheet.drawFrame(3, gv.getCanvas(), new RectF(playerOffsetX, playerOffsetY, playerOffsetX + 1, playerOffsetY + 1));
+                } else {
+                    //Walk animation
+                    if (animationCount == 0) {
+                        player_sheet.drawFrame(4, gv.getCanvas(), new RectF(playerOffsetX, playerOffsetY, playerOffsetX + 1, playerOffsetY + 1));
+                    } else {
+                        player_sheet.drawFrame(5, gv.getCanvas(), new RectF(playerOffsetX, playerOffsetY, playerOffsetX + 1, playerOffsetY + 1));
+                    }
+                }
+                break;
+        }
+        walking = false;
     }
 
     @Override
     public void onRightKey() {
-        if (x == rightBoundary){
-            return;
+        if (x > rightBoundary - 0.2f) {
+            x = rightBoundary;
         }
-
-        if (!collision.checkForCollision(this.x+0.4f, this.y, 0)){
-            x+=0.2;
+        if (x >= rightBoundary || playerOffsetX < 7) {
+            if (playerOffsetX < game.getWidth() - 1.2f) {
+                playerOffsetX += 0.1f;
+            }
+        } else {
+            if (playerOffsetX != 7) {
+                playerOffsetX = 7;
+            }
+            if (!collision.checkForCollision(this.x + 0.3f, this.y, 0)) {
+                x += 0.1f;
+            }
         }
+        this.walkDirection = "right";
+        this.walking = true;
     }
 
     @Override
     public void onLeftKey() {
-        if (x==leftBoundary){
-            return;
+        if (playerOffsetX < 0.2f) {
+            playerOffsetX = 0;
         }
-
-        if (!collision.checkForCollision(this.x-0.4f, this.y,0)){
-            x-=0.2;
+        if (x <= leftBoundary || playerOffsetX > 7) {
+            if (playerOffsetX > 0) {
+                playerOffsetX -= 0.1f;
+            }
+        } else {
+            if (playerOffsetX != 7) {
+                playerOffsetX = 7;
+            }
+            if (!collision.checkForCollision(this.x - 0.2f, this.y, 0)) {
+                x -= 0.1f;
+            }
         }
+        this.walkDirection = "left";
+        this.walking = true;
     }
 
     @Override
     public void onUpKey() {
-        if (y==upperBoundary){
-            return;
+        if (playerOffsetY < 0.2f) {
+            playerOffsetY = 0;
         }
 
-        if (!collision.checkForCollision(this.x, this.y-0.2f, 1)){
-            y-=0.2;
+        if (y <= upperBoundary || playerOffsetY > cameraCenterY) {
+            if (playerOffsetY > 0) {
+                playerOffsetY -= 0.1f;
+            }
+        } else {
+            if (playerOffsetY != cameraCenterY) {
+                playerOffsetY = cameraCenterY;
+            }
+            if (!collision.checkForCollision(this.x, this.y - 0.2f, 1)) {
+                y -= 0.1f;
+            }
         }
+        this.walkDirection = "up";
+        this.walking = true;
     }
 
     @Override
     public void onDownKey() {
-        if (x==downBoundary){
-            return;
+        if (playerOffsetY > game.getHeight() - 0.2f) {
+            playerOffsetY = game.getHeight();
         }
 
-        if (!collision.checkForCollision(this.x, this.y+1.1f, 1)){
-            y+=0.2;
+        if (y >= downBoundary || playerOffsetY < cameraCenterY) {
+            if (playerOffsetY < game.getHeight() - 1.2f) {
+                playerOffsetY += 0.1f;
+            }
+        } else {
+            if (playerOffsetY != cameraCenterY) {
+                playerOffsetY = cameraCenterY;
+            }
+            if (!collision.checkForCollision(this.x, this.y + 1.1f, 1)) {
+                y += 0.1f;
+            }
         }
+        this.walkDirection = "down";
+        this.walking = true;
     }
 
     @Override
@@ -133,13 +250,19 @@ public class Player extends Entity implements KeyListener {
     }
 
     /**
-     *
      * @param x x to check
      * @param y y to check
      * @return true if there is a collision, false when there is not
      */
-    private boolean checkCollisions(float x, float y){
+    private boolean checkCollisions(float x, float y) {
         //return this.game.getEntity(Tiles.class).returnTile(x, y, 2) != 0;
+        return false;
+    }
+
+    private boolean checkBoundary() {
+        if (y == downBoundary || y == upperBoundary || x == leftBoundary || x == rightBoundary) {
+            return true;
+        }
         return false;
     }
 
--- app/src/main/java/nl/saxion/playground/animon/game/menu/MenuItem.java
+++ app/src/main/java/nl/saxion/playground/animon/game/menu/MenuItem.java
@@ -32,7 +32,7 @@ public abstract class MenuItem extends Entity {
 
         this.p = new Paint();
         this.p.setColor(Color.BLACK);
-        this.p.setTextSize(0.5f);
+        this.p.setTextSize(1);
         this.p.setTypeface(pokemonfont);
         this.p.setAntiAlias(true);
         this.p.setTextAlign(Paint.Align.CENTER);
@@ -42,7 +42,12 @@ public abstract class MenuItem extends Entity {
     //Drawing the text
     public void draw(GameView gv) {
         if (onscreenMenu.isIsmenuactive()){
-            gv.getCanvas().drawText(name, posX, posY, p);
+
+            float scaleFactor = 0.5f;
+            gv.getCanvas().save();
+            gv.getCanvas().scale(scaleFactor,scaleFactor);
+            gv.getCanvas().drawText(name, posX/scaleFactor, posY/scaleFactor, p);
+            gv.getCanvas().restore();
         }
     }
 }
