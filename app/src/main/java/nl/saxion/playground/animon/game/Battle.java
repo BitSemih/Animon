afd320c34954485437cbdf22327891b117b48d55
BitSemih <semihoren@hotmail.nl>
2019-06-20 16:54:45 +0200

    Made battle ui components scalable

--- app/src/main/java/nl/saxion/playground/animon/game/Battle.java
+++ app/src/main/java/nl/saxion/playground/animon/game/Battle.java
@@ -8,6 +8,8 @@ import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 import nl.saxion.playground.animon.R;
 import nl.saxion.playground.animon._lib.Entity;
 import nl.saxion.playground.animon._lib.GameView;
@@ -23,7 +25,7 @@ public class Battle extends Entity implements KeyListener {
     private Bitmap backgroundBitmap, platformBitmap, messageBoxBitmap, whatWillPlayerDoBitmap, attackMoveBoxBitmap, statHpBar, statHpBarFiller, statPlayerBitmap, statNpcBitmap;
     private Game game;
     private String welcomeMessage;
-    private float frame, fightX, animonsX, runX;
+    private float frame, fightX, animonsX, runX, w, h;
     private Typeface pokemonfont;
     private Paint p;
     private char[] welcomeMessageLetters;
@@ -32,6 +34,7 @@ public class Battle extends Entity implements KeyListener {
     private float[] menuSelectorPositions = new float[3], attackMovesXPositions = new float[4];
     private boolean isBattleActive = false;
     private boolean isBattleOngoing = false;
+    private ArrayList<AttackMove> playerAttackMoves = new ArrayList<>();
 
     public Battle(int background, Game game, int battlePlatform, int messageBox, Typeface pokemonfont) {
         this.game = game;
@@ -47,18 +50,23 @@ public class Battle extends Entity implements KeyListener {
         this.p.setAntiAlias(true);
         this.p.setTextAlign(Paint.Align.CENTER);
         this.p.setLinearText(true);
-        welcomeMessage = "";
-        String s = "A wild Mees appeared!";
-        this.welcomeMessageLetters = s.toCharArray();
 
         this.game.getEntity(KeyEntity.class).addKeyListener(this);
 
         this.playerAnimon = game.getEntity(Chicken.class);
         this.npcAnimon = game.getEntity(Bear.class);
 
+        welcomeMessage = "";
+        String s = "A wild " + npcAnimon.getName() + " appeared!";
+        this.welcomeMessageLetters = s.toCharArray();
+
         isBattleActive = true;
 
         this.game.setState(state);
+        this.w = game.getWidth();
+        this.h = game.getHeight();
+
+        this.playerAttackMoves = playerAnimon.getAttackMoves();
     }
 
     public void startBattle(Animon playerAnimon, Animon npcAnimon) {
@@ -90,15 +98,15 @@ public class Battle extends Entity implements KeyListener {
 
         gv.drawBitmap(messageBoxBitmap, 0, 0, 1, 1);
 
-        gv.drawBitmap(messageBoxBitmap, game.getWidth() - 1, 0, 1, 1);
+        gv.drawBitmap(messageBoxBitmap, w - 1, 0, 1, 1);
 
-        gv.drawBitmap(messageBoxBitmap, 0, game.getHeight() - 1, 1, 1);
+        gv.drawBitmap(messageBoxBitmap, 0, h - 1, 1, 1);
 
-        gv.drawBitmap(messageBoxBitmap, game.getWidth() / 2 - 0.5f, game.getHeight() / 2 - 0.5f, 1,1);
+        gv.drawBitmap(messageBoxBitmap, w / 2 - 0.5f, h / 2 - 0.5f, 1,1);
 
-        gv.drawBitmap(messageBoxBitmap, game.getWidth() - 1, game.getHeight() - 1, 1, 1);
+        gv.drawBitmap(messageBoxBitmap, w - 1, h - 1, 1, 1);
 
-        /*
+
         if (state == 1) {
             if (backgroundBitmap == null || platformBitmap == null) {
                 backgroundBitmap = gv.getBitmapFromResource(background);
@@ -112,11 +120,11 @@ public class Battle extends Entity implements KeyListener {
                 statPlayerBitmap = gv.getBitmapFromResource(R.drawable.s_battle_stat_player_background);
                 statNpcBitmap = gv.getBitmapFromResource(R.drawable.s_battle_stat_npc_background);
             }
-            gv.drawBitmap(backgroundBitmap, 0, 0, game.getWidth(), game.getHeight());
-            gv.drawBitmap(platformBitmap, game.getWidth() - 7.5f, game.getHeight() * 0.5f, 7, 2);
-            gv.drawBitmap(platformBitmap, 0.5f, game.getHeight() - 5, 8, 2);
+            gv.drawBitmap(backgroundBitmap, 0, 0, w, h);
+            gv.drawBitmap(platformBitmap, w - 7.5f, h * 0.3f, 7, 2);
+            gv.drawBitmap(platformBitmap, 0.5f, h * 0.75f, 8, 2);
             if (!isBattleOngoing) {
-                gv.drawBitmap(messageBoxBitmap, 0, game.getHeight() - 4, game.getWidth(), 4);
+                gv.drawBitmap(messageBoxBitmap, 0, h * 0.80f, w, h*0.2f);
             }
 
             float scaleFactor = 0.5f;
@@ -127,83 +135,79 @@ public class Battle extends Entity implements KeyListener {
 
             if (!nextMessageTrigger || messageDelay < 10) {
                 //Display the welcome message to a battle
-                gv.getCanvas().drawText(welcomeMessage, 2 + (welcomeMessage.length() * 0.25f) / scaleFactor, (game.getHeight() - 2.5f) / scaleFactor, p);
+                gv.getCanvas().drawText(welcomeMessage, 2 + (welcomeMessage.length() * 0.25f) / scaleFactor, h * 0.9f / scaleFactor, p);
             } else if (!isBattleOngoing) {
                 // Player is presented with options after welcome message
                 if (fightX == 0 || animonsX == 0 || runX == 0) {
                     fightX = 2.5f / scaleFactor;
-                    animonsX = (game.getWidth() / 2) / scaleFactor;
-                    runX = (game.getWidth() - 2.5f) / scaleFactor;
+                    animonsX = (w / 2) / scaleFactor;
+                    runX = (w - 2.5f) / scaleFactor;
                     menuSelectorPositions[0] = 1;
                     menuSelectorPositions[1] = animonsX - 5f;
                     menuSelectorPositions[2] = runX - 3;
                 }
-                gv.getCanvas().drawText("FIGHT", fightX, (game.getHeight() - 2.5f) / scaleFactor, p);
-                gv.getCanvas().drawText("ANIMONS", animonsX, (game.getHeight() - 2.5f) / scaleFactor, p);
-                gv.getCanvas().drawText("RUN", runX, (game.getHeight() - 2.5f) / scaleFactor, p);
-                gv.drawBitmap(menuSelectorBitmap, menuSelectorPositions[currentSelector], (game.getHeight() * 2) - 6, 1, 1);
+                gv.getCanvas().drawText("FIGHT", fightX, h * 0.9f / scaleFactor, p);
+                gv.getCanvas().drawText("ANIMONS", animonsX, h * 0.9f / scaleFactor, p);
+                gv.getCanvas().drawText("RUN", runX, h * 0.9f / scaleFactor, p);
+                gv.drawBitmap(menuSelectorBitmap, menuSelectorPositions[currentSelector], h * 0.865f / scaleFactor, h * 0.07f, h * 0.07f);
             } else if (isBattleOngoing) {
                 //When player has selected FIGHT! option
 
-
-
-                /*
                 //if attackmovespositions is not filled with the positions of the attack move options for the selector
                 if (attackMovesXPositions[0] == 0) {
-                    attackMovesXPositions[0] = game.getWidth() - 3;
-                    attackMovesXPositions[1] = game.getWidth() - 3;
-                    attackMovesXPositions[2] = game.getWidth() + 6;
-                    attackMovesXPositions[3] = game.getWidth() + 6;
+                    attackMovesXPositions[0] = w - 3;
+                    attackMovesXPositions[1] = w - 3;
+                    attackMovesXPositions[2] = w + 6;
+                    attackMovesXPositions[3] = w + 6;
                 }
 
                 //What will player do? message box
-                gv.drawBitmap(whatWillPlayerDoBitmap, 0, (game.getHeight() - 4) / scaleFactor, game.getWidth() - 4, 8);
-                gv.getCanvas().drawText("What will", 3.5f / scaleFactor, (game.getHeight() - 2.5f) / scaleFactor, p);
-                gv.getCanvas().drawText(" you do ?", 3 / scaleFactor, (game.getHeight() - 1.5f) / scaleFactor, p);
+                gv.drawBitmap(whatWillPlayerDoBitmap, 0, (h * 0.8f) / scaleFactor, w - 4, h*0.4f);
+                gv.getCanvas().drawText("What will", 3.5f / scaleFactor, (h * 0.88f) / scaleFactor, p);
+                gv.getCanvas().drawText(" you do ?", 3 / scaleFactor, (h * 0.93f) / scaleFactor, p);
 
                 //Attack move option box
-                gv.drawBitmap(attackMoveBoxBitmap, game.getWidth() - 4, (game.getHeight() - 4) / scaleFactor, game.getWidth() + 4, 8);
+                gv.drawBitmap(attackMoveBoxBitmap, w - 4, (h * 0.8f) / scaleFactor, w + 4, h*0.4f);
 
                 //Draw attackmove options for the player
-                gv.getCanvas().drawText(playerAnimon.getAttackMoves().get(0).getMoveName(), game.getWidth() + 1, (game.getHeight() - 2.5f) / scaleFactor, p);
-                gv.getCanvas().drawText(playerAnimon.getAttackMoves().get(1).getMoveName(), game.getWidth() + 1, (game.getHeight() - 1.5f) / scaleFactor, p);
-                gv.getCanvas().drawText(playerAnimon.getAttackMoves().get(2).getMoveName(), game.getWidth() * 2 - 5, (game.getHeight() - 2.5f) / scaleFactor, p);
-                gv.getCanvas().drawText(playerAnimon.getAttackMoves().get(3).getMoveName(), game.getWidth() * 2 - 5, (game.getHeight() - 1.5f) / scaleFactor, p);
+                gv.getCanvas().drawText(playerAttackMoves.get(0).getMoveName(), w + 1, (h * 0.88f) / scaleFactor, p);
+                gv.getCanvas().drawText(playerAttackMoves.get(1).getMoveName(), w + 1, (h * 0.93f) / scaleFactor, p);
+                gv.getCanvas().drawText(playerAttackMoves.get(2).getMoveName(), w * 2 - 5, (h * 0.88f) / scaleFactor, p);
+                gv.getCanvas().drawText(playerAttackMoves.get(3).getMoveName(), w * 2 - 5, (h * 0.93f) / scaleFactor, p);
 
                 //draw the NPC stats
-                gv.drawBitmap(statNpcBitmap, 1, game.getHeight()*0.6f, game.getWidth()*1.1f, 6);
-                gv.getCanvas().drawText(npcAnimon.getName().toUpperCase(), 5, game.getHeight() * 0.7f, p);
-                gv.getCanvas().drawText("Lv" + npcAnimon.getLevel(), game.getWidth()-3, game.getHeight() * 0.7f, p);
+                gv.drawBitmap(statNpcBitmap, 1, h*0.3f, w*1.1f, h * 0.3f);
+                gv.getCanvas().drawText(npcAnimon.getName().toUpperCase(), 5, h * 0.4f, p);
+                gv.getCanvas().drawText("Lv" + npcAnimon.getLevel(), w-3, h * 0.4f, p);
 
                 //draw health stats for npc
-                gv.drawBitmap(statHpBar, 3.5f, game.getHeight()*0.78f, game.getWidth()-3f, 1);
-                gv.drawBitmap(statHpBarFiller, 6.6f, game.getHeight()*0.795f, 9.5f * (npcAnimon.getHealth()/npcAnimon.getMaxHealth()), 0.4f);
+                gv.drawBitmap(statHpBar, 3.5f, h*0.45f, w-3f, 1);
+                gv.drawBitmap(statHpBarFiller, 6.6f, h*0.465f, 9.5f * (npcAnimon.getHealth()/npcAnimon.getMaxHealth()), 0.4f);
 
                 //draw the player stats
-                gv.drawBitmap(statPlayerBitmap,game.getWidth()*0.9f ,  game.getHeight()* 1.2f, game.getWidth()*1.1f, 6);
-                gv.getCanvas().drawText(playerAnimon.getName().toUpperCase(), game.getWidth()* 1.3f, game.getHeight()*1.3f, p);
-                gv.getCanvas().drawText("Lv" + playerAnimon.getLevel(), game.getWidth()* 1.7f, game.getHeight()*1.3f, p);
+                gv.drawBitmap(statPlayerBitmap,w*0.9f ,  h* 1.2f, w*1.1f, h * 0.3f);
+                gv.getCanvas().drawText(playerAnimon.getName().toUpperCase(), w* 1.3f, h*1.3f, p);
+                gv.getCanvas().drawText("Lv" + playerAnimon.getLevel(), w* 1.7f, h*1.3f, p);
 
 
                 //draw health stats for player
-                gv.drawBitmap(statHpBar, game.getWidth() + 1.6f, game.getHeight() * 1.32f, game.getWidth()-3f, 1);
-                gv.drawBitmap(statHpBarFiller, game.getWidth() + 4.7f, game.getHeight() * 1.335f, 9.5f * (playerAnimon.getHealth()/playerAnimon.getMaxHealth()), 0.37f);
-                gv.getCanvas().drawText((int) playerAnimon.getHealth() + "/" + (int) playerAnimon.getMaxHealth(), game.getWidth() * 1.65f, game.getHeight()* 1.42f, p);
+                gv.drawBitmap(statHpBar, w + 1.6f, h * 1.32f, w-3f, 1);
+                gv.drawBitmap(statHpBarFiller, w + 4.7f, h * 1.335f, 9.5f * (playerAnimon.getHealth()/playerAnimon.getMaxHealth()), 0.37f);
+                gv.getCanvas().drawText((int) playerAnimon.getHealth() + "/" + (int) playerAnimon.getMaxHealth(), w * 1.65f, h * 0.72f / scaleFactor, p);
 
                 //Draw animon bitmaps
-                gv.drawBitmap(npcAnimon.getBitmap(), game.getWidth()*1.31f, game.getHeight()-4, 6,6);
-                gv.drawBitmap(playerAnimon.getBitmap(), 5, game.getHeight()*1.3f, 6,6);
+                gv.drawBitmap(npcAnimon.getBitmap(), w*1.31f, h*0.4f, h*0.4f,h*0.4f);
+                gv.drawBitmap(playerAnimon.getBitmap(), 5, h*1.2f, h*0.4f,h*0.4f);
 
                 //Check where the selector is positioned
                 if (currentSelector == 0 || currentSelector == 2) {
-                    gv.drawBitmap(menuSelectorBitmap, attackMovesXPositions[currentSelector], (game.getHeight() - 3) / scaleFactor, 1, 1);
+                    gv.drawBitmap(menuSelectorBitmap, attackMovesXPositions[currentSelector], (h * 0.85f) / scaleFactor, h * 0.07f, h * 0.07f);
                 } else if (currentSelector == 1 || currentSelector == 3) {
-                    gv.drawBitmap(menuSelectorBitmap, attackMovesXPositions[currentSelector], (game.getHeight() - 2) / scaleFactor, 1, 1);
+                    gv.drawBitmap(menuSelectorBitmap, attackMovesXPositions[currentSelector], (h * 0.90f) / scaleFactor, h * 0.07f, h * 0.07f);
                 }
             }
             gv.getCanvas().restore();
         }
-        */
     }
 
     public void addLetterToWelcomeMessage() {
