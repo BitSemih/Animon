594249120d7f98d7a92309c0ed5872dffd50185f
JulienRuumpol <jruumpol@hotmail.com>
2019-06-20 08:32:49 +0200

    Changed title screen music

--- app/src/main/java/nl/saxion/playground/animon/MainActivity.java
+++ app/src/main/java/nl/saxion/playground/animon/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mainMenuTheme = MediaPlayer.create(getApplicationContext(),R.raw.title_screen);
+        mainMenuTheme = MediaPlayer.create(getApplicationContext(),R.raw.title_screen_music);
         mainMenuTheme.start();
         mainMenuTheme.setLooping(true);
 
